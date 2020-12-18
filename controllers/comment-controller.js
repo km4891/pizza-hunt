97dcafd54655c44e9b2d24a3eb14683ee0f1068f
km4891 <kmorarity@gmail.com>
2020-12-17 23:44:33 -0800

    replies finished

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -56,6 +56,15 @@ const commentController = {
             res.json(dbPizzaData);
           })
           .catch(err => res.json(err));
+      },
+      removeReply({ params }, res) {
+          Comment.findOneAndUpdate(
+              { _id: params.commentId },
+              { $pull: { replies: { replyId: params.replyId } } },
+              { new: true }
+          )
+          .then(dbPizzaData => res.json(dbPizzaData))
+          .catch(err => res.json(err));
       }
   };
 
